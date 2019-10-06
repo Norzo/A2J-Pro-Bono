5c11e01bc3e680e7316140a480bff7314b427017
Markus Andhøy <psyma13@nottingham.ac.uk>
2019-10-06 16:08:20 +0100

    First Commit

---
+++ A2J/A2J/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using A2J.Models;
+
+namespace A2J.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}

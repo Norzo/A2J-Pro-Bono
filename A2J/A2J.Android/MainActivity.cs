5ef1c6589c64af1de6613cde715c1cfcfa1021dd
Lawrence Warren <psyljw@nottingham.ac.uk>
2020-02-05 15:02:10 +0000

    Done for today: MainPage & MovingOutPage layout and style fixes; No landscape rotation on android. TODO: merge branches to combine more pages; add content into pages

--- A2J/A2J.Android/MainActivity.cs
+++ A2J/A2J.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace A2J.Droid
 {
-    [Activity(Label = "A2J", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "A2J", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
